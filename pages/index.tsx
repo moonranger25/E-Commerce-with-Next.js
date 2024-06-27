bec83ef2c4c2ed98995365fc1c6a699e3c910dbe
moonranger25 <sr.markhenry610@gmail.com>
2024-06-27 16:07:25 -0500

    e-commerce project with next.js and typescript

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { specialOfferProductsActions } from "../store/specialOfferProducts-slice";
+import { newestProductsActions } from "../store/newestProduct-slice";
+
+import { client } from "../lib/client";
+
+import Carousel from "../components/carousel";
+
+import { IProduct } from "../lib/types/products";
+import { newestProductsFn } from "../utilities/sortByTimeStamp";
+
+const Home: NextPage<{ products: IProduct[] }> = ({ products }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    //add products to offers list
+    const offersProducts = products.filter((item) => item.discount);
+    dispatch(specialOfferProductsActions.addProducts(offersProducts));
+
+    //add products to newest list
+    const sortedProductsByTimeStamp = newestProductsFn(products);
+    dispatch(newestProductsActions.addProducts(sortedProductsByTimeStamp));
+  }, [dispatch, products]);
+
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const productQuery = `*[_type=='product']`;
+  const products = await client.fetch(productQuery);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
