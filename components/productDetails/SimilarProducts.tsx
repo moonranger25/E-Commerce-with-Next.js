04181f32c737055a0a87ba7e7491367e0ac90346
moonranger25 <sr.markhenry610@gmail.com>
2024-07-08 19:29:33 -0500

    Add CarouselBox Component with responsive settings and language support

--- components/productDetails/SimilarProducts.tsx
+++ components/productDetails/SimilarProducts.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { IProduct } from "../../lib/types/products";
 import CarouselBox from "../UI/CarouselBox/CarouselBox";
 import CarouselBoxCard from "../UI/CarouselBox/CarouselBoxCard";
+import { IProduct } from "../../lib/types/products";
 
 interface Props {
   products: IProduct[];
 }
-
-const SimilarProducts: React.FC<Props> = ({
-  products,
-}: {
-  products: IProduct;
-}) => {
-  return (<div>
-    <CarouselBox title="similarProducts" full={true}>
-        {products.map((product) => {
-            <CarouselBoxCard key={product.slug.current} product={product}/>
-        })}
-    </CarouselBox>
-  </div>);
+const SimilarProducts: React.FC<Props> = ({ products }) => {
+  return (
+    <div>
+      <CarouselBox title="similarProducts" full={true}>
+        {products.map((product) => (
+          <CarouselBoxCard key={product.slug.current} product={product} />
+        ))}
+      </CarouselBox>
+    </div>
+  );
 };
 
 export default SimilarProducts;
